13d3255ee58b2cdc972a16001df5e13f0d373213
rami-shirif <4v6n6cwrf7@privaterelay.appleid.com>
2026-09-26 19:44:19 +0100

    Refactor firebase.js to remove auth functionality
    Removed authentication functions and related code.

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously } from "firebase/auth";
-import { getDatabase, ref, get, set, push, remove } from "firebase/database";
+import { getDatabase, ref, get, set, remove } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD_mEgfFs7KBY20rK-rWhSAlzYl3DwPgdk",
@@ -13,14 +12,11 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
 export const db = getDatabase(app);
 
 export async function connectFirebase() {
-  if (!auth.currentUser) {
-    await signInAnonymously(auth);
-  }
-  return auth.currentUser;
+  return db;
 }
 
 export async function readCollection(path) {
